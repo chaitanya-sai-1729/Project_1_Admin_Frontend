c7d18a040a18615f5fe7208a5820a295e563e003
chaitu <chaitanyasai.m.5@gmail.com>
2023-06-06 09:42:44 +0530

    after styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,7 +31,7 @@ import {
   HStack,
   useColorModeValue,
 
-  
+
 
 
 
@@ -52,7 +52,7 @@ function Login() {
 
   const isError = input === ''
 
-  
+
 
   const [showPassword, setShowPassword] = useState(false);
 
@@ -91,87 +91,87 @@ function Login() {
     <>
 
       <ChakraProvider>
-<Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-<Flex
-      minH={'100vh'}
-      width="50%"
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-        <Heading fontSize={'4xl'} textAlign={'center'}>
-            Welcome to Rider'sRealm 
-          </Heading>
-          <br />
-          <Heading fontSize={'3xl'} textAlign={'center'}>
-            Login
-          </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}
-          width="450px">
-          <Stack spacing={4}>
-            
-              <Box>
-                <FormControl id="Username" isRequired>
-                  <FormLabel>Username</FormLabel>
-                  <Input type="text" onChange={(e)=>setInput(e.target.value)} />
-                </FormControl>
+        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+          <Flex
+            minH={'100vh'}
+            width="50%"
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+              <Stack align={'center'}>
+                <Heading fontSize={'4xl'} textAlign={'center'}>
+                  Welcome to Rider'sRealm
+                </Heading>
+                <br />
+                <Heading fontSize={'3xl'} textAlign={'center'}>
+                  Login
+                </Heading>
+                <Text fontSize={'lg'} color={'gray.600'}>
+                  to enjoy all of our cool features ✌️
+                </Text>
+              </Stack>
+              <Box
+                rounded={'lg'}
+                bg={useColorModeValue('white', 'gray.700')}
+                boxShadow={'lg'}
+                p={8}
+                width="450px">
+                <Stack spacing={4}>
+
+                  <Box>
+                    <FormControl id="Username" isRequired>
+                      <FormLabel>Username</FormLabel>
+                      <Input type="text" onChange={(e) => setInput(e.target.value)} />
+                    </FormControl>
+                  </Box>
+
+
+
+                  <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <Input type={showPassword ? 'text' : 'password'} onChange={(e) => setPassword(e.target.value)} />
+                      <InputRightElement h={'full'}>
+                        <Button
+                          variant={'ghost'}
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }>
+                          {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                  <Stack spacing={10} pt={2}>
+                    <Button
+                      loadingText="Submitting"
+                      size="lg"
+                      bg={'blue.400'}
+                      color={'white'}
+                      _hover={{
+                        bg: 'blue.500',
+                      }}
+                      onClick={handleLogin}
+                    >
+                      Login
+                    </Button>
+                  </Stack>
+
+                </Stack>
               </Box>
-              
-            
-            
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} onChange={(e)=>setPassword(e.target.value)} />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-                onClick={handleLogin}
-                >
-                Login
-              </Button>
             </Stack>
-            
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
-      <Flex flex={1}>
-        <Image
-          alt={'Login Image'}
-          objectFit={'cover'}
-          src={
-            'https://c4.wallpaperflare.com/wallpaper/216/720/893/motorcycle-start-biker-ride-wallpaper-preview.jpg'
-          }
-        />
-      </Flex>
-    </Stack>
+          </Flex>
+          <Flex flex={1}>
+            <Image
+              alt={'Login Image'}
+              objectFit={'cover'}
+              src={
+                'https://c4.wallpaperflare.com/wallpaper/216/720/893/motorcycle-start-biker-ride-wallpaper-preview.jpg'
+              }
+            />
+          </Flex>
+        </Stack>
 
       </ChakraProvider>
     </>
